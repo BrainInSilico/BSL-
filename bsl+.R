676d2d63b967ea51412763dec8d5892a0095412a
Vorghyrn <nbeaume@Cyrano.local>
2017-06-09 14:00:08 +0200

    première version bsl+

---
+++ bsl+.R
@@ -0,0 +1,51 @@
+######################## bsl+ ######################
+#
+# creation date : 09/06/2017
+# last update : 09/06/2017
+# author : Nicolas Beaume (nicolasbeaume.consultancy@gmail.com)
+#
+# description : run a simulation of population using BSL (Shiori Yabe, Hiroyoshi Iwata, and Jean-Luc Jannink)
+# with extra features
+#
+#####################################################
+
+#******************** functions *********************
+
+# install a package
+# adapted from http://r.789695.n4.nabble.com/Install-package-automatically-if-not-there-td2267532.html
+load.fun <- function(x) {
+  print(paste("*** installing package", x, "****"))
+  if(isTRUE(x %in% .packages(all.available=TRUE))) {
+    eval(parse(text=paste("require(", x, ")", sep="")))
+  } else {
+    install.packages(x, repos="https://cloud.r-project.org/")
+    eval(parse(text=paste("require(", x, ")", sep="")))
+  }
+} 
+
+installBSL <- function() {
+  load.fun("devtools")
+  install_github("syabe/BreedingSchemeLanguage")  
+}
+#******************** main **************************
+# test if the package is installed, in case not, install it
+if(!isTRUE("BreedingSchemeLanguage" %in% .packages(all.available=TRUE))) {
+  installBSL()
+}
+library(BreedingSchemeLanguage)
+
+#** run simulation **
+
+# simulate phenotipic selection
+defineSpecies(nSim = 5)
+initializePopulation()
+phenotype()
+select()
+cross()
+phenotype()
+select()
+cross()
+phenotype()
+select()
+cross()
+plotData()
